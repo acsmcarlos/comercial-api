81f3e791539be5c476ad05fa4632981e98e3c95e
acsmcarlos <acsmcarlos@gmail.com>
2020-06-19 11:54:16 -0300

    3º commit - sequencia da aula

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -1,9 +1,12 @@
 package com.algaworks.comercial.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,18 @@ public class OportunidadeController {
 		return oportunidades.findAll(); 	//Select na tabela do banco de dados.
 	
 	}
-
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
+		Optional<Oportunidade> oportunidade = oportunidades.findById(id);
+		
+		if(oportunidade.isEmpty()) { 					//se estiver vazio. 
+			return ResponseEntity.notFound().build();	// vamos retornar um response entity (notfund = status 404)
+		}
+		
+		return ResponseEntity.ok(oportunidade.get());
+	}
+	
+	//inclusao de oportunidades na nossa API
+	
 }
